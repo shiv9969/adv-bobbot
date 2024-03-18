dc30ddad4fb49b2b513768db2504c2944958fdb4
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-03-18 20:48:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ IS_STREAM = bool(STREAM_BIN and STREAM_URL)
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'easysky.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '05acd87bcfd1ef4d7425d8bb440de937ab9bccc6')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'krishnalink.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'd9f1f83a2393abb3e8774ab0d7a0465a578f59a9')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
